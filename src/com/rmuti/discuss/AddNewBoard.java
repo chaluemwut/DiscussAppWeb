942ed35f6f49cb7f41711f53ffd1f86b259d6e82
nuttapon12 <nut_tapon_12@hotmail.com>
2015-02-13 16:36:44 +0700

    addnewboard

--- src/com/rmuti/discuss/AddNewBoard.java
+++ src/com/rmuti/discuss/AddNewBoard.java
@@ -26,9 +26,8 @@ public class AddNewBoard extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		
-		String test = "";
+		// TODO Auto-generated method stub		
+	
 	}
 
 	/**
